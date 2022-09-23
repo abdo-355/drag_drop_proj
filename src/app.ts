229cb00c37863e1c6bdc7ccdb0fe17d217394ece
abdo-355 <abdoluxor366@gmail.com>
2022-09-23 18:42:37 +0200

    add new abstract component class to git rid of repetitive code

--- src/app.ts
+++ src/app.ts
@@ -98,6 +98,41 @@ const autoBind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
   return adjDescriptor;
 };
 
+// base component class
+abstract class component<T extends HTMLElement, U extends HTMLElement> {
+  template: HTMLTemplateElement;
+  host: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.template = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.host = document.getElementById(hostId)! as T;
+
+    const importedNode = document.importNode(this.template.content, true);
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+
+    this.attach(insertAtStart);
+  }
+
+  private attach = (atStart: boolean) => {
+    this.host.insertAdjacentElement(
+      atStart ? "afterbegin" : "beforeend",
+      this.element
+    );
+  };
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
 //project input class
 class ProjectInput {
   template: HTMLTemplateElement;
